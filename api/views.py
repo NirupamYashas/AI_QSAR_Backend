a4736f0210d86dcb82c7d6cc83fe64796e716a46
NirupamYashas <48911001+NirupamYashas@users.noreply.github.com>
2023-12-22 12:15:01 -0500

    updated based on comments

--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from api import ai_qsar_model
+import pandas as pd
 
+# Define valid species and maximum file size
 valid_species = ["Goats", "Swine", "Sheep", "Chickens", "Turkeys", "Cattle"]
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5 MB, for example
 
 def is_valid_cas(cas):
     # CAS format: number-number-number
@@ -33,6 +36,12 @@ def singleinputDatapost(request):
 
 @api_view(['POST'])
 def fileinputDatapost(request):
+    # Check the content length from the request
+    content_length = request._request.META.get('CONTENT_LENGTH', 0)
+
+    if int(content_length) > MAX_FILE_SIZE:
+        return Response("File size exceeds the maximum limit.", status=400)
+    
     # [{"CAS": "75184-71-3", "Species": "Chicken"},{"CAS": "317-34-0", "Species": "Cattle"}]
     data = []
     results = []
@@ -40,6 +49,9 @@ def fileinputDatapost(request):
     for item in request.data:
         CAS = item.get('CAS')
         Species = item.get('Species')
+
+        if CAS is None or Species is None:
+            return Response("Missing 'CAS' or 'Species' in the uploaded data", status=400)
 
         if not is_valid_cas(CAS):
             return Response(f"Invalid CAS Number format: {CAS}", status=400)
@@ -55,6 +67,7 @@ def fileinputDatapost(request):
         results.append({'CAS': data[i][0], 'Species': data[i][1], 'LambdaZHl': "{:.2f}".format(y_preds_v2[i][0])})
 
     return Response(results)
+
 
 # @api_view(['POST'])
 # def singleinputDatapost(request):
