60fb45e045994eceae2fa87a8cad8a1cea0f693c
NirupamYashas <48911001+NirupamYashas@users.noreply.github.com>
2024-01-13 17:38:02 -0500

    testing-3

--- AI_QSAR_Backend/settings.py
+++ AI_QSAR_Backend/settings.py
@@ -28,10 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['api', 'localhost', '127.0.0.1', '[::1]']
 # ALLOWED_HOSTS = []
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    '*'
 ]
 
 # Application definition
